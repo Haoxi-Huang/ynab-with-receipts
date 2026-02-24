6e0695f7de1448981c49a9fd872f0f0f53422d6b
Haoxi Huang <haoxi98@gmail.com>
2026-02-23 19:39:23 -0800

    Update to Rails 8, add credentials

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_02_23_184304) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_23_184304) do
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -40,26 +40,26 @@ ActiveRecord::Schema[7.0].define(version: 2026_02_23_184304) do
   end
 
   create_table "receipts", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "store_name"
     t.decimal "amount"
-    t.date "receipt_date"
+    t.datetime "created_at", null: false
     t.text "notes"
-    t.string "ynab_transaction_id"
+    t.date "receipt_date"
+    t.string "store_name"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.string "ynab_account_name"
-    t.string "ynab_payee_name"
     t.integer "ynab_amount"
     t.date "ynab_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "ynab_payee_name"
+    t.string "ynab_transaction_id"
     t.index ["user_id"], name: "index_receipts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "name"
-    t.string "google_uid"
     t.datetime "created_at", null: false
+    t.string "email"
+    t.string "google_uid"
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
